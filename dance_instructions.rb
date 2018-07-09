2b6dac3b8dc306bb0da5916b82d6bbd9b64a2fb8
KristellaBui <kristellabui@gmail.com>
2018-07-09 21:06:05 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,5 +2,5 @@
 # Code your methods below
 
 def starting_stance
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Plant legs far apart, bend knees slightly and keep posture loose, lift right foot"
 end
