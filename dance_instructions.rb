164f15c346da36e119a2df5323d1183e9fc3e80f
KristellaBui <kristellabui@gmail.com>
2018-07-09 21:10:02 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -7,8 +7,10 @@ def starting_stance
   puts "return right foot to the ground"
   puts "finishing with a small skip-step backward"
 end
+
+
 def skip_step
-  puts "lower left foot to the ground"
+  puts "lower left foot to the ground, briefly"
   puts "bounce left foot back up slightly, kicking it a few inches back"
   puts "left, right, left and left"
 end 
